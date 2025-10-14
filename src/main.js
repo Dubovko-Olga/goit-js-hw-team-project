8f59ddd5e831b592de6774c8ac7a8b70376b4aed
olga-dubovko <dubovkoolga70@gmail.com>
2025-10-14 15:38:27 +0200

    add .

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
-import { initFeedbacks } from './js/feedbacks.js';
-import { initHeroSlider } from './js/hero.js';
-import './js/header.js';
-import './js/books/books.js';
 
-import './js/events.js';
-import './js/modal.js';
-
-import './js/scrollup.js';
-import './js/footer';
-import './js/book-modal.js';
+import { initFeedbacks } from './js/feedbacks.js';  
+import { initHeroSlider } from './js/hero.js';     
+import './js/header.js';                          
+import './js/books/books.js';                      
+import './js/events.js';                           
+import './js/modal.js';                          
+import './js/scrollup.js';                         
+import './js/footer';                            
+import './js/book-modal.js';                       
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  initFeedbacks();
-  initHeroSlider();
+  initFeedbacks();  
+  initHeroSlider(); 
 });
-
